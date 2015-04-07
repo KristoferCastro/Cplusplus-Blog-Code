a74a63a8cfb0dce20e969483f960effb4a38d6da
kkc47 <kkc47@sfu.ca>
2015-04-07 14:29:58 -0700

    initial files

---
+++ myfirst.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // this one is a superset and from c++
+//#include<stdio.h> // this one is from c
+int main()
+{
+  using namespace std; // so we dont have to go std::cout all the time
+  cout << "Come up and C++ me some time.";
+  cout << endl;
+  cout << "You won't regret it!" << endl;
+  return 0;  
+}
